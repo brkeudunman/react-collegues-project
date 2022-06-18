0bf0afd1771613a864639b27f0c2219b2013b8a2
Berke Udunman <berkeudunman@gmail.com>
2022-06-18 18:51:29 +0300

    v2

--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react'
+import NavbarButton from './NavbarButton'
+
 
 function Navbar() {
+ 
+  var jsonArr = [{to:"/",text:"Home"},{to:"/notifications",text:"Notifications"}]
+
+  const [selectedIndex, setselectedIndex] = useState(0)
+
   return (
-    <div className='bg-white'>
-      <Link to="/" className='flex items-center text-gray-500 hover:text-white hover:bg-primary-500 active:bg-primary-500 rounded py-1.5 ml-2 mt-3 mx-2    lg:pr-52 md:pr-40 sm:pr-16 pr-4'><i class="ml-2 text-lg fa-solid fa-house"></i><p className='ml-2 mr-10'>Home</p></Link>
-      <Link to="/notifications" className='flex items-center text-gray-500 hover:text-white hover:bg-primary-500 active:bg-primary-500 rounded py-1.5 ml-2 mt-3 mx-2 lg:pr-52 md:pr-40 sm:pr-16 pr-4'><i class="ml-2 text-lg fa-solid fa-file-invoice"></i><p className='ml-2'>Notifications</p></Link>
+    <div className='bg-white md:mr-0 sm:mr-20 mr-0'>
+      {
+        jsonArr.map((element,index)=>{
+          return (<NavbarButton onClickFunction={setselectedIndex(index)} elementTo={element.to} elementText={element.text} isSelected={selectedIndex}/>)
+        })
+      }
+      
     </div>
   )
 }
