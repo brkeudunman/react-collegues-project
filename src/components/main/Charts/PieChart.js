64a98aa80732c8638de7f362f413f7ee1c8713ec
Berke Udunman <berkeudunman@gmail.com>
2022-06-25 12:38:17 +0300

    colors have been added

--- src/components/main/Charts/PieChart.js
+++ src/components/main/Charts/PieChart.js
@@ -4,7 +4,7 @@ import {Pie} from 'react-chartjs-2'
 function PieChart({data,header}) {
   return (
     
-    <div className='flex flex-col items-center flex-1 bg-white rounded-lg border-not-metal p-4 shadow-md mt-4'>
+    <div className='flex flex-col items-center bg-white rounded-lg border-not-metal p-4 shadow-md mt-4'>
         {header}
         <Pie data={data} />
     </div>
