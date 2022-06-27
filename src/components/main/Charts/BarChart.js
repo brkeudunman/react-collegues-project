41a95c07c0f9e9dd2c3f417be13e6d417e99df78
Berke Udunman <berkeudunman@gmail.com>
2022-06-27 21:13:29 +0300

    bar chart height problem fixed

--- src/components/main/Charts/BarChart.js
+++ src/components/main/Charts/BarChart.js
@@ -3,7 +3,7 @@ import {Bar} from 'react-chartjs-2'
 
 function BarChart({data,header}) {
   return (
-    <div className='flex flex-col justify-center bg-white rounded-lg border-not-metal px-2 shadow-md mt-4 h-full'>
+    <div className='flex flex-col justify-center bg-white rounded-lg border-not-metal px-2 shadow-md mt-4'>
         {header}
         <Bar data={data} />
     </div>
