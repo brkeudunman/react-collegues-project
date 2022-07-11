e13109c342e8fae5adb3cf0f4875a252fe082031
Berke Udunman <berkeudunman@gmail.com>
2022-07-11 13:46:00 +0300

    navbar clients ico changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import NavbarButton from './NavbarButton'
 function Navbar() {
  
   var jsonArr = [{to:"/",text:"Home"},{to:"/notifications",text:"Notifications"},{to:"/charts",text:"Charts"},{to:"/contact",text:"Contact Us"},{to:"/clients",text:"Clients"}]
-  var icoArr = [(<i class="ml-2 sm:text-lg fa-solid fa-house"></i>),(<i class="ml-2 text-lg fa-solid fa-book"></i>),(<i class="ml-2 text-lg fa-solid fa-chart-bar"></i>),(<i class="ml-2 text-lg fa-solid fa-address-book"></i>),(<i class="ml-2 text-lg fa-solid fa-address-book"></i>)]
+  var icoArr = [(<i class="ml-2 sm:text-lg fa-solid fa-house"></i>),(<i class="ml-2 text-lg fa-solid fa-book"></i>),(<i class="ml-2 text-lg fa-solid fa-chart-bar"></i>),(<i class="ml-2 text-lg fa-solid fa-address-book"></i>),(<i class="ml-2 text-lg fa-solid fa-people-group"></i>)]
  
 
   return (
